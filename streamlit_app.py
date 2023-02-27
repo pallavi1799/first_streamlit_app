3de8e06f02e8686d974fd0e6257fab9ea40c9074
pallavi1799 <125975031+pallavi1799@users.noreply.github.com>
2023-02-27 20:06:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,21 +24,21 @@ fruits_to_show = my_fruit_lis.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+#def get_fruityvice_data(this_fruit_choice):
+#  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
+#  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#  return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select a fruit to get information")
-  else:
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+ #   streamlit.error("please select a fruit to get information")
+#  else:
+ #   back_from_function=get_fruityvice_data(fruit_choice)
+  #  streamlit.dataframe(back_from_function)
+#except URLError as e:
+ # streamlit.error()
   
   
   
@@ -48,8 +48,8 @@ except URLError as e:
 streamlit.header ("The fruit load list contains:")
 def get fruit load list():
   with my_cnx.cursor () as my_cur:
-       my_cur. execute("select * from fruit load list")
-       return my_cur.fetchall()
+    my_cur. execute("select * from fruit load list")
+    return my_cur.fetchall()
 if streamlit.button ('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"1)
    my_data_rows = get_fruit_load_list()
