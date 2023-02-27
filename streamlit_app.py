59c23aee9a615a33e58749546acebd0dd5dddd13
pallavi1799 <125975031+pallavi1799@users.noreply.github.com>
2023-02-27 20:08:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,47 +23,31 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_lis.i
 fruits_to_show = my_fruit_lis.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
-#def get_fruityvice_data(this_fruit_choice):
-#  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
-#  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#  return fruityvice_normalized
-
-#streamlit.header("Fruityvice Fruit Advice!")
-#try:
-#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#  if not fruit_choice:
- #   streamlit.error("please select a fruit to get information")
-#  else:
- #   back_from_function=get_fruityvice_data(fruit_choice)
-  #  streamlit.dataframe(back_from_function)
-#except URLError as e:
- # streamlit.error()
-  
-  
-  
-
-  
-  
-streamlit.header ("The fruit load list contains:")
-def get fruit load list():
-  with my_cnx.cursor () as my_cur:
-    my_cur. execute("select * from fruit load list")
-    return my_cur.fetchall()
-if streamlit.button ('Get Fruit Load List'):
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"1)
-   my_data_rows = get_fruit_load_list()
-   streamlit.dataframe (my_data_rows)
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+     streamlit.error("please select a fruit to get information")
+   else:
+     back_from_function=get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
+except URLError as e:
+   streamlit.error()
   
 streamlit.stop()
 
       
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#treamlit.dataframe(fruityvice_normalized)
 
 
 
