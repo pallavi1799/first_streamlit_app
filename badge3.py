6a506fbbf37aa2c370a64af0fc2293ed248d2902
pallavi1799 <125975031+pallavi1799@users.noreply.github.com>
2023-03-31 12:42:30 +0530

    Update badge3.py

--- badge3.py
+++ badge3.py
@@ -11,5 +11,5 @@ import streamlit
 streamlit.text('You will be Cordially Invited to the Exchanges')
 streamlit.text('...as soon as you submit your Account Locator')
 streamlit.text('...using the Streamlit form LINKED HERE!')
-streamlit.text('gold embellishment')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
